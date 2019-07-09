1f55a2781c02579e70f5c216f475cf3c38362412
iAquino47 <ianaquino47@gmail.com>
2019-07-09 13:11:05 +0100

    added /cat

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   'Hello world!'
 end
 
 get '/secret' do
   'This is a secret message. Well done for finding it! Here is a medal 🏅'
+end
+
+get '/cat' do
+  "<div style = 'border:3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+    </div>"
 end
